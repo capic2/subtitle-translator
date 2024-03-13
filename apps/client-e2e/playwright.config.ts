dd58e82e8da7e78bf5549960e03af8eaf22fc656
Vincent <vcapic@gmail.com>
2024-03-13 13:12:16 +0100

    playwright report

--- apps/client-e2e/playwright.config.ts
+++ apps/client-e2e/playwright.config.ts
@@ -17,6 +17,7 @@ const baseURL = process.env['BASE_URL'] || 'http://localhost:4200';
  */
 export default defineConfig({
   ...nxE2EPreset(__filename, { testDir: './src' }),
+  reporter: [['html', { outputFolder: 'playwright' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     baseURL,
